da84abfceb2bdab250b45dc426be9d1890527da1
Francesco <francescococlite@gmail.com>
2021-09-15 18:49:01 +0200

    push a caso

--- app/Post.php
+++ app/Post.php
@@ -11,6 +11,10 @@ class Post extends Model
         'title',
         'slug',
         'content',
+        'category_id'
 
     ];
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,16 +2,17 @@
 @section('content')
 <form action="{{route('admin.posts.update', $post->id)}}" method="post">
   @if($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li> 
-                    @endforeach
-                </ul>
-            </div>
-    @endif
-    @csrf
-    @method('PATCH')
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li> 
+            @endforeach
+        </ul>
+    </div>
+  @endif
+  
+  @csrf
+   @method('PATCH')
   <div class="mb-3">
     <label for="titolo" class="form-label">Titolo</label>
     <input type="text" name="title" class="form-control" value="{{$post->title}}" id="titolo" aria-describedby="emailHelp">
@@ -19,7 +20,7 @@
   <div class="form-floating">
     <label for="descrizione" class="form-label">Descrizione</label>
     <textarea class="form-control" name="content" id="descrizione" style="height: 100px">{{$post->content}}</textarea>
-</div>
-  <button type="submit" class="btn btn-primary mt-5">Submit</button>
-</form>
+  </div>
+    <button type="submit" class="btn btn-primary mt-5">Submit</button>
+  </form>
 @endsection
